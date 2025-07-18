37d8d2e82e96ae98c241059336072c1abb58c638
thienanonLinux <huynhdothienan@gmail.com>
2025-07-18 21:09:29 +0700

    initial commit

---
+++ fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set -g fish_greeting
+alias blackwhite="hyprshade on /home/wozzy/.config/hypr/shaders/greyscale.glsl"
+alias bluelightfilter='hyprshade on /home/wozzy/.config/hypr/shaders/bluelightfilter.glsl'
